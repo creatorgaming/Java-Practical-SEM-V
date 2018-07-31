fab2b5e034ae437227d2016684062bfc28f7a538
Divyanshu Tyagi <divyanshutyagi8@gmail.com>
2018-07-31 22:34:42 +0530

    Simple Expression Calculation Done

--- ExpressionCalculator/src/dev/project/expCal/calculation.java
+++ ExpressionCalculator/src/dev/project/expCal/calculation.java
@@ -1,59 +1,101 @@
 package dev.project.expCal;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class calculation {
 	
 	public String expression;
+	public ArrayList<String>numbers = new ArrayList<String>();
+	public ArrayList<String>oper = new ArrayList<String>();
 	
 	public void input() {
 		System.out.print("Enter the expression : ");
+		@SuppressWarnings("resource")
 		Scanner inp = new Scanner(System.in);
 		expression = inp.next();
-		
-		System.out.print("Result using Simple expression calculation : ");
-		System.out.println(simple_calculate());
-		System.out.print("Result using rules of expression calculation : ");
-		dmas_calculate();
+			
+		System.out.println("Result using Simple expression calculation : " + simple_calculate());
+//		System.out.print("Result using BODMAS of expression calculation : " + dmas_calculate());
 	}
-	
-	public int simple_calculate() {
-		boolean check_digit_1 = false, check_digit_2 = true;
-		int digit_1 = Integer.MIN_VALUE, digit_2 = Integer.MIN_VALUE;
-		char operator = ' ';
-		String digit_1_s = "" , digit_2_s = "";
-		int result = 0;
+
+	public void create_lists() {
+		
+		String number = "", operator = "";
 		
 		for(char ch : expression.toCharArray()) {
-			if((int)ch >= 48 && (int)ch <= 57) {
-				if(!check_digit_1 && check_digit_2) {
-					digit_1 = Integer.parseInt(digit_1_s);
-					digit_2_s = digit_2_s + "" + ch;
-				}else if(check_digit_2 && !check_digit_2){
-						digit_2 = Integer.parseInt(digit_2_s);
-						digit_1_s = digit_1_s + "" + ch;
-				}
-			}else if(digit_1 != Integer.MIN_VALUE && digit_2 != Integer.MIN_VALUE){
-				
-				if(operator == '+') {
-					result = digit_1 + digit_2;
-				}else if(operator == '-') {
-					result = digit_1 - digit_2;
-				}else if(operator == '/') {
-					result = digit_1 / digit_2;
-				}else if(operator == '*') {
-					result = digit_1 * digit_2;
-				}
-				
-				digit_1 = result;
-				digit_2 = Integer.MIN_VALUE;
+//			System.out.println(ch);
+			if(ch >= '0' && ch <= '9') {
+				number += ch;
+			}else if (ch == '-' && ( operator.equals("*") || operator.equals("/") ) ) {
+				number += ch;
 			}else {
-				operator = ch;
+				operator = "" + ch;
+				numbers.add(number);
+				oper.add(operator);
+				number = "";
 			}
 		}
-		
+		if(number != "") {
+			numbers.add(number);
+		}
+//		System.out.println("List of numbers   : " + numbers);
+//		System.out.println("List of operators : " + oper);
+	}
+	
+	public double calculate(double digit_1, double digit_2, String oper) {
+		double result = 0.00;
+		switch (oper) {
+			case "+":
+				result = digit_1 + digit_2;
+				break;
+
+			case "-":
+				result = digit_1 - digit_2;
+				break;
+
+			case "*":
+				result = digit_1 * digit_2;
+				break;
+
+			case "/":
+				result = digit_1 / digit_2;
+				break;
+
+			case "%":
+				result = digit_1 % digit_2;
+				break;
+
+			default:
+				System.out.println("!!! WRONG OPERATOR !!!");
+				System.exit(1);
+				break;
+		}
 		return result;
 	}
 	
-	public void dmas_calculate() {}
+	public double simple_calculate() {
+		create_lists();
+		double result = 0.00, digit_1 = 0.00, digit_2 = 0.00;
+		int i = 0;
+		digit_1 = Integer.parseInt(numbers.get(0));
+		for (i = 1; i  < numbers.size(); i++) {
+			digit_2 = Integer.parseInt(numbers.get(i));
+			digit_1 = calculate(digit_1, digit_2, oper.get(i-1));
+		}
+		
+		if(i - 1 != oper.size()) {
+			System.out.println("!!! Illegal Expression !!!");
+			System.exit(1);
+		}
+		
+		result = digit_1;
+		return result;
+	}
+	
+	public double dmas_calculate() {
+		double result = 0.00;
+		return result;
+		
+	}
 }
--- ExpressionCalculator/src/dev/project/expCal/expCal.java
+++ ExpressionCalculator/src/dev/project/expCal/expCal.java
@@ -1,7 +1,20 @@
 package dev.project.expCal;
 
+//import javax.script.ScriptEngine;
+//import javax.script.ScriptEngineManager;
+//import javax.script.ScriptException;
+
 public class expCal {
 	public static void main(String[] args) {
 		new calculation().input();
+//	    ScriptEngineManager mgr = new ScriptEngineManager();
+//	    ScriptEngine engine = mgr.getEngineByName("JavaScript");
+//	    String foo = "3-10";
+//	    try {
+//			System.out.println(engine.eval(foo));
+//		} catch (ScriptException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} 
 	}
 }
