e3b6c9c9bb131236fd4b9c86770336c066d28da5
Divyanshu Tyagi <divyanshutyagi8@gmail.com>
2018-08-06 11:02:45 +0530

    BODMAS Functionality added

--- ExpressionCalculator/src/dev/project/expCal/calculation.java
+++ ExpressionCalculator/src/dev/project/expCal/calculation.java
@@ -1,42 +1,50 @@
 package dev.project.expCal;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class calculation {
 	
 	public String expression;
 	public ArrayList<String>numbers = new ArrayList<String>();
-	public ArrayList<String>oper = new ArrayList<String>();
+	public ArrayList<Character>oper = new ArrayList<Character>();
 	
 	public void input() {
 		System.out.print("Enter the expression : ");
 		@SuppressWarnings("resource")
 		Scanner inp = new Scanner(System.in);
 		expression = inp.next();
-			
-//		System.out.println("Result using Simple expression calculation : " + simple_calculate());
-		System.out.print("Result using BODMAS of expression calculation : " + bodmas_calculate());
+
+		try {
+//			System.out.println("Result using Simple expression calculation : " + simple_calculate());
+			System.out.print("Result using BODMAS of expression calculation : " + bodmas_calculate());
+		}catch(Exception e) {
+			System.out.println("Exception " + e + " caught.");
+		}
 	}
 
 	public void create_lists() {
 		
-		String number = "", operator = "";
+		String number = "";
+		char operator = 0;
 		
 		for(char ch : expression.toCharArray()) {
 //			System.out.println(ch);
+			while(ch == ' ');
+			
 			if (ch >= '0' && ch <= '9') {
 				number += ch;
-			}else if (ch == '-' && ( operator.equals("*") || operator.equals("/") ) ) {
+			}else if (ch == '-' && ( operator == '*' || operator == '/' ||  operator == '%' || operator == '+')) {
 				number += ch;
 			}else if (ch == '.' && !number.isEmpty()) {
 				number += ch;
 			}else {
-				if (number.endsWith(".") || StringUtils.countOccurencesOf(number,".") > 2) {
-					System.out.println("!!! Invalid number Detected !!!");
-					System.exit(1);
-				}
-				operator = "" + ch;
+//				if (number.endsWith(".")) {
+//					System.out.println("!!! Invalid number Detected !!!");
+//					System.exit(1);
+//				}
+				operator = ch;
 				numbers.add(number);
 				oper.add(operator);
 				number = "";
@@ -46,30 +54,30 @@ public class calculation {
 			numbers.add(number);
 		}
 
-//		System.out.println("List of numbers   : " + numbers);
-//		System.out.println("List of operators : " + oper);
+		System.out.println("List of numbers   : " + numbers);
+		System.out.println("List of operators : " + oper);
 	}
 	
-	public double calculate(double digit_1, double digit_2, String oper) {
+	public double calculate(double digit_1, double digit_2, char oper) {
 		double result = 0.00;
 		switch (oper) {
-			case "+":
+			case '+':
 				result = digit_1 + digit_2;
 				break;
 
-			case "-":
+			case '-':
 				result = digit_1 - digit_2;
 				break;
 
-			case "*":
+			case '*':
 				result = digit_1 * digit_2;
 				break;
 
-			case "/":
+			case '/':
 				result = digit_1 / digit_2;
 				break;
 
-			case "%":
+			case '%':
 				result = digit_1 % digit_2;
 				break;
 
@@ -102,11 +110,43 @@ public class calculation {
 	
 	public double bodmas_calculate() {
 		double result = 0.00;
-		create_lists();
-		for (String string : numbers) {
+		String number = "";
+		LinkedList<Character> operator = new LinkedList<Character>();
+		LinkedList<Double> list = new LinkedList<Double>();
+		for (char ch : expression.toCharArray()) {
+			System.out.println("Number   List --> " + list);
+			System.out.println("Operator List --> " + operator);
+			while(ch == ' ');
 			
+			if(ch == '-' && (number.isEmpty())) {
+				number += ch;
+			}else if (ch >= '0' && ch <= '9') {
+				while(ch >= '0' && ch <= '9')
+					number += ch;
+				list.push(Double.parseDouble(number));
+			}else if(ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '%') {
+				while(!operator.isEmpty() && hasPrecedence(ch, operator.peek()))
+					list.push(calculate(list.pop(), list.pop(), operator.pop()));
+				operator.push(ch);
+			}
 		}
-		return result;
+		while (!operator.isEmpty()) {
+			list.push(calculate(list.pop(), list.pop(), operator.pop()));
+		}
 		
+		if(!list.isEmpty()) {
+			result = list.pop();
+		}else {
+			System.out.println("!!! Invalid expression !!!");
+		}
+		
+		return result;
+	}
+	
+	public boolean hasPrecedence(char oper_1, char oper_2) {
+		if((oper_1 == '*' || oper_1 == '/') || (oper_2 == '+' || oper_2 == '-'))
+			return false;	
+		else 
+			return true;
 	}
 }
--- File meta data displayer/src/dev/project/fileMetaData/propDisplayer.java
+++ File meta data displayer/src/dev/project/fileMetaData/propDisplayer.java
@@ -1,6 +1,6 @@
 package dev.project.fileMetaData;
 
-import java.io.File;
+import java.io.File; 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
