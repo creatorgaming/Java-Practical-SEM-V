21f688a4f30d64e053310895b9f55ba086ea077b
Divyanshu Tyagi <divyanshutyagi8@gmail.com>
2018-08-08 11:23:40 +0530

    calculation.java Updated

--- ExpressionCalculator/src/dev/project/expCal/calculation.java
+++ ExpressionCalculator/src/dev/project/expCal/calculation.java
@@ -6,9 +6,10 @@ import java.util.Scanner;
 
 public class calculation {
 	
-	public String expression;
-	public ArrayList<String>numbers = new ArrayList<String>();
-	public ArrayList<Character>oper = new ArrayList<Character>();
+	private String expression;
+	private boolean dmasFlag = false;
+	private ArrayList<String>numbers = new ArrayList<String>();
+	private ArrayList<Character>oper = new ArrayList<Character>();
 	
 	public void input() {
 		System.out.print("Enter the expression : ");
@@ -18,10 +19,11 @@ public class calculation {
 
 		try {
 //			System.out.println("Result using Simple expression calculation : " + simple_calculate());
-			System.out.print("Result using BODMAS of expression calculation : " + bodmas_calculate());
+			System.out.print("Result using BODMAS of expression calculation : " + bodmas_calculate());		
 		}catch(Exception e) {
 			System.out.println("Exception " + e + " caught.");
 		}
+		
 	}
 
 	public void create_lists() {
@@ -58,7 +60,7 @@ public class calculation {
 		System.out.println("List of operators : " + oper);
 	}
 	
-	public double calculate(double digit_1, double digit_2, char oper) {
+	public double calculate(double digit_1, double digit_2, char oper, boolean dmasFlag) {
 		double result = 0.00;
 		switch (oper) {
 			case '+':
@@ -74,7 +76,11 @@ public class calculation {
 				break;
 
 			case '/':
-				result = digit_1 / digit_2;
+				if(dmasFlag) {
+					result = digit_2 / digit_1;
+				}else {
+					result = digit_1 / digit_2;	
+				}
 				break;
 
 			case '%':
@@ -93,10 +99,11 @@ public class calculation {
 		create_lists();
 		double result = 0.00, digit_1 = 0.00, digit_2 = 0.00;
 		int i = 0;
+		
 		digit_1 = Double.parseDouble(numbers.get(0));
 		for (i = 1; i  < numbers.size(); i++) {
 			digit_2 = Double.parseDouble(numbers.get(i));
-			digit_1 = calculate(digit_1, digit_2, oper.get(i-1));
+			digit_1 = calculate(digit_1, digit_2, oper.get(i-1),dmasFlag);
 		}
 		
 		if(i - 1 != oper.size()) {
@@ -114,36 +121,45 @@ public class calculation {
 		char[] exp = expression.toCharArray();
 		LinkedList<Character> operator = new LinkedList<Character>();
 		LinkedList<Double> list = new LinkedList<Double>();
-// Debuging		
-//		list.push(2.00);
-//		list.push(3.00);
-//		list.push(5.00);
-//		System.out.println(list);
-		
+		dmasFlag = true;
 		for (int i = 0; i < exp.length; i++) {
-			System.out.println(exp[i] + " <-index-> " + i + "\nNumber   List --> " + list);
-			System.out.println("Operator List --> " + operator);
+//			System.out.println(exp[i] + " <-index-> " + i + "\nNumber   List --> " + list);
+//			System.out.println("Operator List --> " + operator);
 			while(exp[i] == ' ');
-			
-			if(exp[i] == '-' && number.isEmpty() && (exp[i-1] >= '0' && exp[i-1] <= '9')) {
+			 
+			if(exp[i] == '-' && (operator.isEmpty() && !(exp[i-1] >= '0' && exp[i-1] <= '9' ) )) {
 				number += exp[i];
-			}else if (exp[i] >= '0' && exp[i] <= '9') {				
-				while(exp[i] >= '0' && exp[i] <= '9') {
-					number += exp[i++];
-				}				
+			}else if ( ( exp[i] >= '0' && exp[i] <= '9' ) || exp[i] == '.') {				
+				while( i < exp.length && ( ( exp[i] >= '0' && exp[i] <= '9' ) || (exp[i] == '.') ) ) {
+//					System.out.println("Current exp --> " + exp[i]);
+					if ( exp[i] == '.' && number.matches("[0-9]*") ) {
+						if(!number.contains(".")) {
+							number += exp[i++];
+						}else {
+							System.out.println("!!! INVALID EXPRESSION !!!");
+							System.exit(1);
+						}
+					}else {
+						number += exp[i++];
+					}
+				}	
+				i--;
+				System.out.println("Number entered --> " + number);
 				list.push(Double.parseDouble(number));
 				number = "";
 			}else if(exp[i] == '+' || exp[i] == '-' || exp[i] == '*' || exp[i] == '/' || exp[i] == '%') {
-				System.out.println(exp[i]);
 				while(!operator.isEmpty() && hasPrecedence(exp[i], operator.peek()))
-					list.push(calculate(list.pop(), list.pop(), operator.pop()));
+					list.push(calculate(list.pop(), list.pop(), operator.pop(),dmasFlag));
 				operator.push(exp[i]);
 			}
 		}
-		while (!operator.isEmpty()) {
-			list.push(calculate(list.pop(), list.pop(), operator.pop()));
-		}
 		
+		System.out.println("\nNumber   List --> " + list);
+		System.out.println("Operator List --> " + operator);
+
+		while (!operator.isEmpty()) {
+			list.push(calculate(list.pop(), list.pop(), operator.pop(),dmasFlag));
+		}		
 		if(!list.isEmpty()) {
 			result = list.pop();
 		}else {
