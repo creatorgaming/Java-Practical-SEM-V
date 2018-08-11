a0cd5ca0d18e38ad0345bdb4df621ca2a37244db
Divyanshu Tyagi <divyanshutyagi8@gmail.com>
2018-08-11 22:29:39 +0530

    File meta data displayer Project Update
    propDisplayer.java Updated

--- File meta data displayer/src/dev/project/fileMetaData/propDisplayer.java
+++ File meta data displayer/src/dev/project/fileMetaData/propDisplayer.java
@@ -1,13 +1,15 @@
 package dev.project.fileMetaData;
 
-import java.io.File; 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.time.ZoneId;
 import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
 
 public class propDisplayer {
 	
@@ -27,8 +29,14 @@ public class propDisplayer {
 		file = new File(path);	
 		if(file.exists()) {
 			if(file.isDirectory()) {
-				displayDirectoryDetails();
+				System.out.println("\nDirectory Properties...");
+				displayDirectoryProperties();
+				System.out.println("\nDirectory Contents...");
+				displayDirectoryContents();
 			}else {
+				System.out.println("\nFile Content...");
+				displayFileContents();
+				System.out.println("\nFile Properties...");
 				displayFileProperties();
 			}
 		}else {
@@ -36,7 +44,7 @@ public class propDisplayer {
 		}
 	}
 	
-	void displayDirectoryDetails() {
+	void displayDirectoryContents() {
 		File[] listOfFiles = file.listFiles();
 		for (File file2 : listOfFiles) {
 			if(file2.isFile()) {
@@ -44,6 +52,55 @@ public class propDisplayer {
 			}else if(file2.isDirectory()) {
 				System.out.println("Directory --> " + file2.getName());
 			}
+		}
+	}
+	
+	long getFolderSize(File file) {
+		long size = 0;
+	    File[] files = file.listFiles();	 
+	    for (int i = 0; i < files.length; i++) {
+	        if (files[i].isFile()) {
+	            size += files[i].length();
+	        }
+	        else {
+	            size += getFolderSize(files[i]);
+	        }
+	    }
+		return size;
+	}
+	
+	void displayDirectoryProperties(){
+		Path path = file.toPath();
+		BasicFileAttributes attributes = null;
+		try {
+			attributes = Files.readAttributes(path, BasicFileAttributes.class);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("File Name	   --> " + file.getName());
+		System.out.println("File Size	   --> " + (getFolderSize(file)/(1024*1024*1024)) + " GB");
+		System.out.println("File Created  On   --> " + attributes.creationTime().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime().toString().replaceAll("T", " "));
+		System.out.println("File Modified On   --> " + attributes.lastModifiedTime().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime().toString().replaceAll("T", " "));
+		System.out.println("File Accessed On   --> " + attributes.lastAccessTime().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime().toString().replaceAll("T", " "));
+		System.out.println("File Absolute Path --> " + file.getAbsolutePath());		
+	}
+	
+	void displayFileContents(){
+		if(file.canRead()) {
+			BufferedReader br;
+			try {
+				br = new BufferedReader(new FileReader(file));
+				String line = null;
+				 while ((line = br.readLine()) != null) {
+				   System.out.println(line);
+				 }
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}else {
+			System.out.println("!!!! Cannot Print the file !!!!");
 		}
 	}
 	
@@ -58,7 +115,14 @@ public class propDisplayer {
 		}
 		System.out.println("File Name	   --> " + file.getName());
 		System.out.println("File Type	   --> " + getFileExtension(file));
-		System.out.println("File Size	   --> " + (file.length()/(1024*1024)) + " MB");
+		if(file.length() > (1024*1024)) {
+			System.out.println("File Size	   --> " + (file.length()/(1024*1024)) + " MB");	
+		}else if (file.length() > (1024)) {
+			System.out.println("File Size	   --> " + (file.length()/(1024)) + " KB");
+		}else {
+			System.out.println("File Size	   --> " + file.length() + " Bytes");
+		}
+		
 		System.out.println("File Created  On   --> " + attributes.creationTime().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime().toString().replaceAll("T", " "));
 		System.out.println("File Modified On   --> " + attributes.lastModifiedTime().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime().toString().replaceAll("T", " "));
 		System.out.println("File Accessed On   --> " + attributes.lastAccessTime().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime().toString().replaceAll("T", " "));
