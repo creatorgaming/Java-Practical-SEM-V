d516d59bb90b5a7481534f6f51508523e3261c66
Divyanshu Tyagi <divyanshutyagi8@gmail.com>
2018-09-07 07:58:45 +0530

    Banking/ Menu.java, BanikingMain.java Updated

--- Banking/src/dev/project/banking/Menu.java
+++ Banking/src/dev/project/banking/Menu.java
@@ -1,5 +1,6 @@
 package dev.project.banking;
 
+
 import java.util.Scanner;
 
 import dev.project.bankingOperation.*;
@@ -9,7 +10,6 @@ public class Menu {
 	static final String password = "1234";
 	
 	public Menu(String person) {
-		System.out.flush();
 		if (person.equalsIgnoreCase("user")) {
 			userMenu();
 		}else if (person.equalsIgnoreCase("admin")) {
@@ -20,8 +20,11 @@ public class Menu {
 	@SuppressWarnings("resource")
 	private void userMenu() {
 		Scanner inp = new Scanner(System.in);
-		System.out.println("\n\n!!!!!!!!!!!!!!!!!! USER !!!!!!!!!!!!!!!!!!");
-		System.out.print("\n\t\tEnter account number : ");
+		System.out.println("\n\n\n\n");
+		System.out.println("\t\t--------------");
+		System.out.println("\t\t|||| USER ||||");
+		System.out.println("\t\t--------------");
+		System.out.print("\n\t   Enter account number : ");
 		int accountNo = inp.nextInt();
 		new User(accountNo);
 	}
@@ -29,8 +32,11 @@ public class Menu {
 	@SuppressWarnings("resource")
 	private void adminMenu() {
 		Scanner inp = new Scanner(System.in);
-		System.out.println("\n\n!!!!!!!!!!!!!!!!!! ADMIN !!!!!!!!!!!!!!!!!!");
-		System.out.print("\n\t\tEnter password : ");
+		System.out.println("\n\n\n");
+		System.out.println("\t\t---------------");
+		System.out.println("\t\t|||| ADMIN ||||");
+		System.out.println("\t\t---------------");
+		System.out.print("\n\tEnter password : ");
 		String pass = inp.next();
 		if (!pass.equals(password)) {
 			System.out.println("!!!! Invalid Password !!!!");
