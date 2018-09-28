5c70137dccecb471a70edbec04cc0f1ee5c18c42
Divyanshu Tyagi <divyanshutyagi8@gmail.com>
2018-09-28 22:25:30 +0530

    Updated

--- SqlQueryRunner/src/dev/project/sqlQueryRunner/MainMenu.java
+++ SqlQueryRunner/src/dev/project/sqlQueryRunner/MainMenu.java
@@ -1,13 +1,31 @@
 package dev.project.sqlQueryRunner;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/mainmenu")
 public class MainMenu extends HttpServlet {
 	
-	public void service(HttpServletRequest _request, HttpServletResponse _response) {
+	public void service(HttpServletRequest _request, HttpServletResponse _response) throws IOException {
 		String optionSelected = _request.getParameter("choice");
-		System.out.println(optionSelected);
+		switch (optionSelected) {
+		case "create":			
+			break;
+		case "insert":			
+			break;
+		case "structure":			
+			break;
+		case "show":			
+			break;
+		default:
+			break;
+		}
+		PrintWriter writer =  _response.getWriter();
+		writer.print(optionSelected);
 	}
 }
