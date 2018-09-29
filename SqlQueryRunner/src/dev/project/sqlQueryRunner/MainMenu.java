2a35c8b1b46a65a8ed7694ee2579e4fbd413c7a4
Divyanshu Tyagi <divyanshutyagi8@gmail.com>
2018-09-29 20:43:44 +0530

    Update in SqlQueryRunner

--- SqlQueryRunner/src/dev/project/sqlQueryRunner/MainMenu.java
+++ SqlQueryRunner/src/dev/project/sqlQueryRunner/MainMenu.java
@@ -3,6 +3,9 @@ package dev.project.sqlQueryRunner;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,21 +14,26 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/mainmenu")
 public class MainMenu extends HttpServlet {
 	
-	public void service(HttpServletRequest _request, HttpServletResponse _response) throws IOException {
+	public void service(HttpServletRequest _request, HttpServletResponse _response) throws IOException, ServletException {
 		String optionSelected = _request.getParameter("choice");
-		switch (optionSelected) {
-		case "create":			
-			break;
-		case "insert":			
-			break;
-		case "structure":			
-			break;
-		case "show":			
-			break;
-		default:
-			break;
+//		switch (optionSelected) {
+//		case "create":			
+//			break;
+//		case "insert":			
+//			break;
+//		case "structure":			
+//			break;
+//		case "show":			
+//			break;
+//		default:
+//			break;
+//		}
+		if (optionSelected.equals("create")) {
+			ServletContext sc = this.getServletContext();
+			RequestDispatcher rd = sc.getRequestDispatcher("/jsp/Create_Table.jsp");
+			rd.include(_request, _response);
 		}
-		PrintWriter writer =  _response.getWriter();
-		writer.print(optionSelected);
+//		PrintWriter writer =  _response.getWriter();
+//		writer.print(optionSelected);
 	}
 }
